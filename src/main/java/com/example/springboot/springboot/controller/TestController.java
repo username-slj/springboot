13e475e74d5e2e26a71883223b7ebacd577c3f92
378333414@qq.com <378333414@qq.com>
2020-06-03 23:19:59 +0800

    @Value

--- src/main/java/com/example/springboot/springboot/controller/TestController.java
+++ src/main/java/com/example/springboot/springboot/controller/TestController.java
@@ -5,10 +5,6 @@ import com.example.springboot.springboot.controller.dto.TestDto;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.couchbase.CouchbaseProperties;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.*;
 
@@ -28,6 +24,7 @@ public class TestController {
 
     /**
      * 接口调通
+     *
      * @param name 入参
      * @return 信息
      */
@@ -39,6 +36,7 @@ public class TestController {
 
     /**
      * 对象打印入参信息
+     *
      * @param testDto 入参对象
      * @return 信息
      */
@@ -51,16 +49,16 @@ public class TestController {
      * 获取属性文件内容
      * 1.实体类获取
      * 2.environment工具类获取
+     *
      * @return 信息
      */
     @RequestMapping(value = "/getPropertiesInfo", method = RequestMethod.GET)
-    public String getPropertiesInfo(){
+    public String getPropertiesInfo() {
         //映射在实体类中
 //        return configInfo.getName()+"=="+configInfo.getAge()+"=="+configInfo.getMessage();
         //使用environment直接获取属性文件中的信息
-        return environment.getProperty("test.name")+"=="+environment.getProperty("test.age")+"=="+environment.getProperty("test.message");
+        return environment.getProperty("test.name") + "==" + environment.getProperty("test.age") + "==" + environment.getProperty("test.message");
     }
-
 
 
 }
