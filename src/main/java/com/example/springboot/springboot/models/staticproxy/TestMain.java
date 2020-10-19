2eae7144458b388e61aef796a0ca22f3440bc216
username-slj <Slj524312^_^>
2020-10-19 17:13:19 +0800

    静态代理

--- src/main/java/com/example/springboot/springboot/models/staticproxy/TestMain.java
+++ src/main/java/com/example/springboot/springboot/models/staticproxy/TestMain.java
@@ -1,5 +1,8 @@
 package com.example.springboot.springboot.models.staticproxy;
 
+/**
+ * 静态代理
+ */
 public class TestMain {
     public static void main(String[] args) {
         DogDaoImpl dogDaoImpl = new DogDaoImpl();
