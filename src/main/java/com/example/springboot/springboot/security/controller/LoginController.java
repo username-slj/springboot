d38ac67abd9b03961d1867b51cd8c75ce2117652
username-slj <Slj524312^_^>
2021-02-04 18:02:33 +0800

    。。。

--- src/main/java/com/example/springboot/springboot/security/controller/LoginController.java
+++ src/main/java/com/example/springboot/springboot/security/controller/LoginController.java
@@ -2,6 +2,8 @@ package com.example.springboot.springboot.security.controller;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,12 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
  * https://blog.csdn.net/qq_42640067/article/details/113062222
  */
 @Api("security")
-@RestController(value = "/security")
+@RestController
+@Slf4j
 public class LoginController {
 
     @ApiOperation(value = "登陆")
-    @RequestMapping("/login")
+    @GetMapping(value = "/login")
     public String login() {
+        log.info("=============================================");
         return "redirect:main.html";
     }
 }
