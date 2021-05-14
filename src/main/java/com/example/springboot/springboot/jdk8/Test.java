ead62c1ebbd72a43bda9a0da309fcf95f93b8c29
username-slj <378333414@qq.com>
2021-05-14 17:55:53 +0800

    CompletableFuture.supplyAsync

--- src/main/java/com/example/springboot/springboot/jdk8/Test.java
+++ src/main/java/com/example/springboot/springboot/jdk8/Test.java
@@ -3,20 +3,26 @@ package com.example.springboot.springboot.jdk8;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.example.springboot.springboot.common.Utils;
-import com.example.springboot.springboot.jdk8.entity.*;
+import com.example.springboot.springboot.jdk8.entity.A;
+import com.example.springboot.springboot.jdk8.entity.A2;
+import com.example.springboot.springboot.jdk8.entity.ListsDTO;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.logging.log4j.core.util.JsonUtils;
 import org.assertj.core.util.Lists;
 import org.springframework.beans.BeanUtils;
 import org.springframework.util.CollectionUtils;
-import org.springframework.util.StringUtils;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.StringJoiner;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import java.util.function.Consumer;
 
 @Slf4j
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         //->写法
 //        test1();
         //无参应用::
@@ -45,11 +51,59 @@ public class Test {
 //        test12();
 //        StringJoiner
 //        test13();
-        test14();
+//        switch 如果第一个条件没有break，第二个语句不会判断直接输出
+//        test14();
+//        CompletableFuture.supplyAsync:异步执行完main在可以执行
+//        test15();
+        test16();
+    }
+
+    private static void test16() {
+
+
+    }
+
+    private static void test15() throws ExecutionException, InterruptedException {
+        CompletableFuture<String> completableFuture1 = CompletableFuture.supplyAsync(() -> {
+            log.info("{} start1,time->{}", Thread.currentThread(), System.currentTimeMillis());
+            if (true) {
+                return "test15";
+            } else {
+                log.info("{} end1,time->{}", Thread.currentThread(), System.currentTimeMillis());
+                return "======";
+            }
+        });
+
+        CompletableFuture<String> completableFuture2 = CompletableFuture.supplyAsync(() -> {
+            log.info("{} start3,time->{}", Thread.currentThread(), System.currentTimeMillis());
+            if (true) {
+                return "test16";
+            } else {
+                log.info("{} end3,time->{}", Thread.currentThread(), System.currentTimeMillis());
+                return "======";
+            }
+        });
+        log.info("run result->{}", completableFuture2.get());
+        log.info("run result->{}", completableFuture1.get());
+        log.info("main thread time->{}", System.currentTimeMillis());
     }
 
     private static void test14() {
-
+        int i = 1;
+        switch (i) {
+            case 1:
+            case 2:
+                log.info("1,2");
+                break;
+            case 3:
+                log.info("3");
+                break;
+            case 4:
+                log.info("4");
+                break;
+            default:
+                ;
+        }
     }
 
     private static void test13() {
@@ -84,51 +138,51 @@ public class Test {
         arrayList.add(a);
 
         List<A> a1 = listsDTO.getA();
-        for(A alist: a1){
-            log.info("info==={},{},{}",alist.getName(),alist.getStatus(),alist.getUniqueNo());
-            log.debug("debug==={},{},{}",alist.getName(),alist.getStatus(),alist.getUniqueNo());
-            log.error("error===={},{},{}",alist.getName(),alist.getStatus(),alist.getUniqueNo());
-            log.warn("warn==={},{},{}",alist.getName(),alist.getStatus(),alist.getUniqueNo());
-            log.trace("trace==={},{},{}",alist.getName(),alist.getStatus(),alist.getUniqueNo());
+        for (A alist : a1) {
+            log.info("info==={},{},{}", alist.getName(), alist.getStatus(), alist.getUniqueNo());
+            log.debug("debug==={},{},{}", alist.getName(), alist.getStatus(), alist.getUniqueNo());
+            log.error("error===={},{},{}", alist.getName(), alist.getStatus(), alist.getUniqueNo());
+            log.warn("warn==={},{},{}", alist.getName(), alist.getStatus(), alist.getUniqueNo());
+            log.trace("trace==={},{},{}", alist.getName(), alist.getStatus(), alist.getUniqueNo());
         }
 
 
     }
 
     private static void test10() {
-        A a=new A();
+        A a = new A();
         a.setUniqueNo(111L);
         a.setName("zhangsan");
         System.out.println(Utils.objectToJson(null));
     }
 
-    private static void test09(){
+    private static void test09() {
         A a = new A();
         a.setName("lisi");
         a.setStatus("1");
         a.setUniqueNo(000002L);
-        List<A> aList=new ArrayList<>();
+        List<A> aList = new ArrayList<>();
         aList.add(a);
 
         A2 a2 = new A2();
-        List<A2> a2List=new ArrayList<>();
-        if(!CollectionUtils.isEmpty(aList)){
-            for(A aLists : aList){
+        List<A2> a2List = new ArrayList<>();
+        if (!CollectionUtils.isEmpty(aList)) {
+            for (A aLists : aList) {
                 BeanUtils.copyProperties(aLists, a2);
                 a2List.add(a2);
             }
-            System.out.println(a2List.get(0).getName()+"==="+a2List.get(0).getStatus()+"==="+a2List.get(0).getUniqueNo());
+            System.out.println(a2List.get(0).getName() + "===" + a2List.get(0).getStatus() + "===" + a2List.get(0).getUniqueNo());
         }
     }
 
-    private static void test08(){
+    private static void test08() {
         A a = new A();
         a.setName("zhangsan");
         a.setStatus("2");
         a.setUniqueNo(0000001L);
         A2 a2 = new A2();
         BeanUtils.copyProperties(a, a2);
-        System.out.println("==="+a2.getName()+"==="+a2.getStatus()+"==="+a2.getUniqueNo());
+        System.out.println("===" + a2.getName() + "===" + a2.getStatus() + "===" + a2.getUniqueNo());
 
     }
 
@@ -136,12 +190,12 @@ public class Test {
         //String是不可变的，“1”符号保存先判断常量池三方存在1，存在1就返回1的引用，不存在就保存后返回引用
         //new String("1") 首先在常量池中判断是否存在1存在就在堆中开辟一块内存存放1，返回堆内存引用
         //                不存在就在常量池中存放，在在堆内存中开辟一块内存存放返回堆内存的引用
-        String a="1";
-        String b="1";
+        String a = "1";
+        String b = "1";
         //引用类型+字符串属于运行时创建（a存放在常量池中，“”存放常量池中是两块所以引用地址不一样）
-        System.out.println(a+"" == b);
+        System.out.println(a + "" == b);
         //字符串量直接保存在常量池中，“1”不是引用类型直接相加“”所以最后引用是一样
-        System.out.println("1"+"" == b);
+        System.out.println("1" + "" == b);
     }
 
     private static void test06() {
@@ -151,7 +205,7 @@ public class Test {
         list.add("115555");
 
         Optional.ofNullable(list).orElse(new ArrayList<>()).forEach(s -> {
-            if(s.length() == 4){
+            if (s.length() == 4) {
                 return;
             }
             System.out.println(s);
